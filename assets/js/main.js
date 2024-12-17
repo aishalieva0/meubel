de3fa641ff28b04b7682cca8ab2ab02b7bb0714d
aishalieva0 <aysalieva6@gmail.com>
2024-12-17 14:01:29 +0400

    search box - dropdown - responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
 const hamburger = document.querySelector('#hamburger');
 const navBar = document.querySelector('#navBar');
+const searchBtn = document.querySelector('#searchBtn');
+const searchBox = document.querySelector('.searchBox');
+const closeIcon = document.querySelector('#closeIcon');
+const searchIcon = document.querySelector('#searchIcon');
 
 hamburger.addEventListener('click', () => {
     navBar.classList.toggle('open');
-    hamburger.classList.toggle('active');
+    hamburger.classList.toggle('opened');
+})
+
+searchBtn.addEventListener('click', () => {
+    searchBox.classList.toggle('activeFlex');
+    if (searchBox.className.includes('activeFlex')) {
+        closeIcon.style.display = 'block'
+        searchIcon.style.display = 'none'
+    } else {
+        closeIcon.style.display = 'none'
+        searchIcon.style.display = 'block'
+    }
 })
